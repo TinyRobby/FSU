1a69a70d8d3830a0dc47898f1a2653cf3e8386cd
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-03-02 19:08:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Chrome extention for reccomending free software over payed
+
+// this for chrome
+let url = 'adobe.com/products/premiere.html' 
+if(url.includes('office.com')) {
+	alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
+}
+if(url.includes('adobe.com/products/premiere.html')){
+	alert('If you dont know already, Adobe Premiere is pretty expensive. I ');
+}
