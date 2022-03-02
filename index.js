28ab5be95d826eea86e44afa5af8041e59438be4
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-03-02 19:12:01 +0000

    added manifest.json

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ if(url.includes('office.com')) {
 	alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
 }
 if(url.includes('adobe.com/products/premiere.html')){
-	alert('If you dont know already, Adobe Premiere is pretty expensive. I ');
+	alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
 }
