6cdaabe874d6d623c8ed5786d7269c5f760543bf
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-04-12 18:21:49 +0000

    added while loop to run query constantly

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-chrome.tabs.query({currentWindow: true, active: true}, 	function(tabs){
-    let tabUrl = tabs[0].url;
-	if(tabUrl.includes('office.com')) {
-	alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
-	}
-	if(tabUrl.includes('adobe.com/products/premiere.html')){
-		alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
-	}
-});
+let amogus = true
+while(amogus == true){
+	chrome.tabs.query({currentWindow: true, active: true}, 	function(tabs){
+    	let tabUrl = tabs[0].url;
+		if(tabUrl.includes('office.com')) {
+			alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
+		}
+		if(tabUrl.includes('adobe.com/products/premiere.html')){
+			alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
+		}
+	});
+}
