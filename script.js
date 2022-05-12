0c085a37a2dd320ef1e577f82dbd61b741eb4f3a
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-05-12 18:16:10 +0000

    added while loop to main script

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let amogus = true
-while(amogus == true){
-	chrome.tabs.query({currentWindow: true, active: true}, 	function(tabs){
-    	let tabUrl = tabs[0].url;
-		if(tabUrl.includes('office.com')) {
-			alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
-		}
-		if(tabUrl.includes('adobe.com/products/premiere.html')){
-			alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
-		}
+while(true) {
+	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    	let url = tabs[0].url;
+    	return url
 	});
+	let tabUrl = tab;
+	if(tabUrl.includes('office.com')) {
+			alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
+	}
+	if(tabUrl.includes('adobe.com/products/premiere.html'))	{
+		alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
+	}
 }
