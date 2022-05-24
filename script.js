ac14e97cb90510c9e4ad1dc9560b3d0955d7f77e
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-05-24 18:08:22 +0000

    UwU

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-while(true) {
+
+setInterval(function(){ 
+    //this code runs every second 
+
+let on = true;
+while(on = true) {
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    	let url = tabs[0].url;
-    	return url
+    	let daUrl = tabs[0].url;
+    	return daUrl;
 	});
-	let tabUrl = tab;
-	if(tabUrl.includes('office.com')) {
+	if(daUrl.includes('office.com')) {
 			alert('If you don\'t know already, Microsoft office is pretty expensive, I\'d reccomend Libreoffice or Openoffice, both free alternataves.')
 	}
-	if(tabUrl.includes('adobe.com/products/premiere.html'))	{
+	if(daUrl.includes('adobe.com/products/premiere.html'))	{
 		alert('If you dont know already, Adobe Premiere is pretty expensive. I\'d recommend OpenShot! A free aternative to most video editors.');
 	}
 }
+}, 500);
